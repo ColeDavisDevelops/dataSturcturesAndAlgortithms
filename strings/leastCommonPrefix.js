e66b0ee34f5efb10987a1e86b58624a9c4280f77
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-09-13 22:18:52 -0500

    pushing more

---
+++ strings/leastCommonPrefix.js
@@ -0,0 +1,73 @@
+var helper = (str1, str2) => {
+  if (str2 === "" || str1 === "") return "";
+  if (str1[0] !== str2[0]) return "";
+  // compare first string to second str and return the LCP, called with the next string,
+  // if no next string return empty string
+    
+    let LCP = str1[0];
+    let counter = 1;
+    while(str1[counter] === str2[counter] && (counter < str1.length || counter < str2.length)) {
+      console.log("curr", str1[counter])
+      LCP += str1[counter];
+      counter++
+    }
+    return LCP
+}
+
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 1) return strs[0];
+  let LCP = "" 
+  if(2 <= strs.length) {
+    LCP = helper(strs[0], strs[1]);
+    if (LCP === '') {
+      return LCP
+    }
+  }
+  for (let i=1; i<strs.length; i++) {
+    console.log('loop')
+    if(i+1 < strs.length) {
+      LCP = helper(LCP, strs[i+1]);
+      if (LCP === '') {
+        return LCP
+      }
+    }
+  }
+  
+  return LCP;
+    
+};
+
+
+[[0,0,1,0,1,1,1,0,1,1],
+ [1,1,1,1,0,1,1,1,1,1],
+ [1,1,1,1,1,0,0,0,1,1],
+ [1,0,1,0,1,1,1,0,1,1],
+ [0,0,1,1,1,0,1,1,1,1],
+ [1,0,1,1,1,1,1,1,1,1],
+ [1,1,1,1,0,1,0,1,0,1],
+ [0,1,0,0,0,1,0,0,1,1],
+ [1,1,1,0,1,1,0,1,0,1],
+ [1,0,1,1,1,0,1,1,1,0]]
+
+[[0,0,1,0,1,2,1,0,1,2],         
+ [1,1,2,1,0,1,1,1,4,5],
+ [2,1,3,1,1,0,0,0,1,2],
+ [1,0,1,0,1,1,1,0,1,2],
+ [0,0,1,1,1,0,1,1,2,4],
+ [1,0,1,2,1,1,1,2,1,4],
+ [1,1,1,1,0,1,0,1,0,1],
+ [0,1,0,0,0,1,0,0,1,2],
+ [1,1,1,0,1,1,0,1,0,1],
+ [1,0,1,1,1,0,1,2,1,0]]
+
+
+[[0,0,1,0,1,2,1,0,1,2],
+ [1,1,2,1,0,1,1,1,2,3],
+ [2,1,2,1,1,0,0,0,1,2],
+ [1,0,1,0,1,1,1,0,1,2],
+ [0,0,1,1,1,0,1,1,2,3],
+ [1,0,1,2,1,1,1,2,1,2],
+ [1,1,1,1,0,1,0,1,0,1],
+ [0,1,0,0,0,1,0,0,1,2],
+ [1,1,1,0,1,1,0,1,0,1],
+ [1,0,1,1,1,0,1,2,1,0]]
