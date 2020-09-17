39f80aff5f4a9642d16e631eb47d9a5cac5f62db
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-09-17 02:51:42 -0500

    memoize

---
+++ memoization/fibNumber.js
@@ -0,0 +1,10 @@
+const cache = {};
+
+const fibNumber = (n) => {
+  if (n < 2) return n;
+  if (cache[n]) return cache[n];
+  
+  const result = fibNumber(n-1) + fibNumber(n-2);
+  cache[n] = result;
+  return resultl;
+}
