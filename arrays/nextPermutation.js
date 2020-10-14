31dd06c2b24e5b7933fbfd00ddfb00a843c1f14b
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-10-13 22:35:08 -0500

    next perm naive

---
+++ arrays/nextPermutation.js
@@ -0,0 +1,17 @@
+const nextPermutation = (nums) => {
+  // find every permutation
+
+
+}
+
+const fact = (num) => {
+
+  let fact = num;
+  num--;
+  while(num !== 0) {
+    fact *= num; 
+    num--;
+  }
+
+  return fact;
+}
