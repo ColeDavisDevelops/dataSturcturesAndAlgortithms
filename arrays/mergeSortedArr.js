73732d029555a4e56c01846e6a13884b844ca546
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-11-01 20:22:25 -0600

    _

--- arrays/mergeSortedArr.js
+++ arrays/mergeSortedArr.js
@@ -5,28 +5,23 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    
+var merge = function (nums1, m, nums2, n) {
   let counter = 0;
-  while(counter < nums1.length-1) {
-    if(nums2[0] < nums1[counter]) {
-
-        let insert = nums2[0];
-        for(let i=counter; i<nums1.length; i++) {
-          let d = nums1[i];
-          nums1[i] = insert;
-          insert = d;
+  while (counter < nums1.length - 1) {
+    if (nums2[0] < nums1[counter]) {
+      let insert = nums2[0];
+      for (let i = counter; i < nums1.length; i++) {
+        let d = nums1[i];
+        nums1[i] = insert;
+        insert = d;
       }
       nums2.shift();
     }
-    counter++
-
-  }    
+    counter++;
+  }
   if (nums2.length > 0) {
-    for(let i=m+n-nums2.length; i<nums1.length; i++) { 
+    for (let i = m + n - nums2.length; i < nums1.length; i++) {
       nums1[i] = nums2.shift();
     }
   }
-
-    
 };
